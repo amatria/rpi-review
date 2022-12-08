209306caadd0155c44e387487d7d60673b43c077
amatria <i.amatria@udc.es>
2022-12-08 02:34:07 +0100

    minor adjustments

--- download-interactions.bash
+++ download-interactions.bash
@@ -1,32 +1,29 @@
 #!/usr/bin/env bash
 
-INPUT=npinter-v5.txt
-NONCODEIDS=ids-noncode-v5.txt
+# START OF CONFIGURATION
 
-# end of configuration
-
+INPUT=data/npinter-v5.txt
+NONCODEIDS=data/ids-noncode-v5.txt
 RNA_OUTPUT=rna.fa
 PROTEIN_OUTPUT=protein.fa
-INTERACTS_OUTPUT=protein-rna-pairs.txt
+INTERACTIONS_OUTPUT=interactions.txt
 
-CACHE=cache
+# END OF CONFIGURATION
 
-TMP_FILE=/var/tmp/tmp_file.txt
-TMP2_FILE=/var/tmp/tmp2_file.txt
-
-[[ ! -f $INPUT ]] || [[ ! -f $NONCODEIDS ]] && echo -e "\033[0;31mFatal error:\033[0m missing input files" && exit 1
+[[ ! -f $INPUT ]] || [[ ! -f $NONCODEIDS ]] && echo -e "\033[0;31mError:\033[0m missing input files" && exit 1
 
 [[ -f $RNA_OUTPUT ]] && rm -rf $RNA_OUTPUT
 [[ -f $PROTEIN_OUTPUT ]] && rm -rf $PROTEIN_OUTPUT
-[[ -f $INTERACTS_OUTPUT ]] && rm -rf $INTERACTS_OUTPUT
+[[ -f $INTERACTIONS_OUTPUT ]] && rm -rf $INTERACTIONS_OUTPUT
+
+CACHE=cache
+declare -A ALLOWED_ORGANISMS=( ["Homo sapiens"]=0 ["Mus musculus"]=0 ["Drosophila melanogaster"]=0 )
 
 mkdir -p $CACHE
 
 touch $RNA_OUTPUT
 touch $PROTEIN_OUTPUT
-touch $INTERACTS_OUTPUT
-
-declare -A ALLOWED_ORGANISMS=( ["Homo sapiens"]=0 ["Mus musculus"]=0 ["Drosophila melanogaster"]=0 )
+touch $INTERACTIONS_OUTPUT
 
 find_protein () {
     [[ "${PROTID}" == "-" ]] || \
@@ -48,7 +45,7 @@ find_protein () {
         IFS=$'\t' read -r -a WORDS <<< $LINE
         local TMP_PROTID="${WORDS[0]}"
         local TMP_ORGANISM="${WORDS[5]}"
-        if grep -q "${ORGANISM}" <<< "${TMP_ORGANISM}"; then
+        if grep -i -q "${ORGANISM}" <<< "${TMP_ORGANISM}"; then
             PROTID="${TMP_PROTID}"
             return 0
         fi
@@ -60,38 +57,34 @@ find_protein () {
 find_lncRNA () {
     [[ "${NONCODEID}" == "-" ]] && return 1
 
-    grep -i -w "${NONCODEID}" $NONCODEIDS > $TMP_FILE
-    [[ ! -s $TMP_FILE ]] && return 1
-
     local LINE
     local WORDS
     local NAMES
     while IFS= read -r LINE; do
         IFS=$'\t' read -r -a WORDS <<< $LINE
         local TMP_NAME="${WORDS[0]}"
-        IFS=$'.' read -r -a NAMES <<< "${TMP_NAME}"
+        IFS="." read -r -a NAMES <<< "${TMP_NAME}"
 
         [[ ! -f "${CACHE}/${TMP_NAME}.html" ]] && curl -s "http://www.noncode.org/show_rna.php?id=${NAMES[0]}&version=${NAMES[1]}" --output "${CACHE}/${TMP_NAME}.html"
-        python3 noncode_html_parser.py "${CACHE}/${TMP_NAME}.html" > $TMP2_FILE
 
-        if grep -q "${ORGANISM}" $TMP2_FILE; then
+        if python3 utils/noncode_html_parser.py "${CACHE}/${TMP_NAME}.html" | grep -i -q "${ORGANISM}"; then
             NONCODEID="${TMP_NAME}"
             return 0
         fi
-    done < $TMP_FILE
+    done < <(grep -i -w "${NONCODEID}" $NONCODEIDS)
 
     return 1
 }
 
 check_duplicate () {
-    if grep -q -P "${PROTID}\t${NONCODEID}\t${ORGANISM}" $INTERACTS_OUTPUT; then
+    if grep -q -P "${PROTID}\t${NONCODEID}\t${ORGANISM}" $INTERACTIONS_OUTPUT; then
         return 1
     fi
     return 0
 }
 
 download_protein () {
-    if grep -q "${PROTID}" $INTERACTS_OUTPUT; then
+    if grep -q "${PROTID}" $INTERACTIONS_OUTPUT; then
         return 0
     fi
 
@@ -109,28 +102,27 @@ download_protein () {
 }
 
 download_lncRNA () {
-    if grep -q "${NONCODEID}" $INTERACTS_OUTPUT; then
+    if grep -q "${NONCODEID}" $INTERACTIONS_OUTPUT; then
         return
     fi
 
-    python3 noncode_html_parser.py "${CACHE}/${NONCODEID}.html" > $TMP_FILE
     echo ">${NONCODEID}" >> $RNA_OUTPUT
-    sed -n '2{p;q}' $TMP2_FILE >> $RNA_OUTPUT
+    python3 utils/noncode_html_parser.py "${CACHE}/${NONCODEID}.html" | sed -n '2{p;q}' >> $RNA_OUTPUT
 }
 
-echo -e "\033[1mlncRNA--protein downloader\033[0m -- by Iñaki Amatria Barral"
+echo -e "\033[1mlncRNA-protein downloader\033[0m -- by Iñaki Amatria Barral"
 
 ANALYZED=0
 TOTAL_LINES=`wc -l $INPUT | awk '{print $1}'`
 
 while IFS= read -r LINE; do
     ANALYZED=$((ANALYZED + 1))
-    echo -ne "\r\033[1mProgress:\033[0m ${ANALYZED}/${TOTAL_LINES}"
+    PROGRESS=`echo "${ANALYZED} ${TOTAL_LINES}" | awk '{printf "%.2f", $1 / $2 * 100}'`
+    echo -ne "\r\033[1mProgress:\033[0m ${PROGRESS}% (${ANALYZED}/${TOTAL_LINES})"
+
+    [[ "${LINE}" == "#"* ]] && continue
 
     IFS=$'\t' read -r -a WORDS <<< $LINE
-
-    [[ "${LINE}" = "#"* ]] && continue
-
     if [[ "${WORDS[3]}" == "lncRNA" ]] && [[ "${WORDS[6]}" == "protein" ]]; then
         PROTID="${WORDS[5]}"
         NONCODEID="${WORDS[2]}"
@@ -159,7 +151,7 @@ while IFS= read -r LINE; do
         [[ "$?" -gt "0" ]] && continue
         download_lncRNA
 
-        echo -e "${PROTID}\t${NONCODEID}\t${ORGANISM}" >> $INTERACTS_OUTPUT
+        echo -e "${PROTID}\t${NONCODEID}\t${ORGANISM}" >> $INTERACTIONS_OUTPUT
     fi
 done < $INPUT
 
