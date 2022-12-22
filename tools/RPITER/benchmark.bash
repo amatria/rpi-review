5513e276824bc0a79d918ec86f84bc72d0db7b76
amatria <i.amatria@udc.es>
2022-12-22 11:14:24 +0100

    kfold + test against npinter

--- tools/RPITER/benchmark.bash
+++ tools/RPITER/benchmark.bash
@@ -8,11 +8,10 @@ DATASETS=( 'rpi2241' 'rpi1807' 'rpi488' 'rpi369' 'npinter-v5' )
 
 source env/bin/activate
 
-cd RPITER
+for (( i=0; i<${#DATASETS[@]}; i++ )); do
+    python RPITER/rpiter.py -d ${DATASETS[$i]} -m ${DATASETS[$i]} 1> /dev/null
+done
+echo
 for (( i=0; i<${#DATASETS[@]}-1; i++ )); do
-    for (( j=0; j<${#DATASETS[@]}; j++ )); do
-        python rpiter.py -d ${DATASETS[$i]} -m ${DATASETS[$j]} 1> /dev/null
-    done
-    echo
-    echo
+    python RPITER/rpiter.py -d npinter-v5 -m ${DATASETS[$i]} -p 1> /dev/null
 done
